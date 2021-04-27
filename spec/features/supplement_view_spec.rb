af93f74d4dee6add501f7cb50ef32935e1f4a2d3
Travis <mckinstrytg1@gmail.com>
2021-04-27 14:08:21 -0600

    Adds notes for next test

--- spec/features/supplement_view_spec.rb
+++ spec/features/supplement_view_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Supplment view page' do
-   it 'index pages' do
+   it 'index page' do
      visit ('/supplements')
      number = (1..402).to_a
      number.each do |num|
@@ -17,7 +17,7 @@ RSpec.describe 'Supplment view page' do
   it 'show page related supplements buttons' do
     visit('/supplements')
     click_button('L-Citrulline DL-Malate 2:1 Powder')
-
+    # expect page to have every Supplement with 'energy' as a category to show up on this page
     click_button('Energy')
     expect(current_path).to eq('/search')
     expect(page).to have_button('Caffeine Pills')
